b82e3427330292239deb2e3d6eb4b4790284800d
hotwolf <github@hotwolf.de>
2015-06-04 09:33:47 +0200

    added backlight driver

--- Software/Source/Asm/backlight_AriCalculator.s
+++ Software/Source/Asm/backlight_AriCalculator.s
@@ -1,7 +1,7 @@
-#ifndef	LED
-#define	LED
+#ifndef	BACKLIGHT
+#define	BACKLIGHT
 ;###############################################################################
-;# S12CBase - LED - LED Driver (AriCalculator)                                 #
+;# S12CBase - BACKLIGHT - Backlight Driver (AriCalculator)                     #
 ;###############################################################################
 ;#    Copyright 2010-2012 Dirk Heisswolf                                       #
 ;#    This file is part of the S12CBase framework for Freescale's S12C(X) MCU  #
@@ -21,10 +21,10 @@
 ;#    along with S12CBase.  If not, see <http://www.gnu.org/licenses/>.        #
 ;###############################################################################
 ;# Description:                                                                #
-;#    This module controls the LED on the OpenBDM Pod.                         #
+;#    This module controls the BACKLIGHT on the OpenBDM Pod.                   #
 ;###############################################################################
 ;# Version History:                                                            #
-;#    January 7, 2015                                                          #
+;#    June 3, 2015                                                             #
 ;#      - Initial release                                                      #
 ;###############################################################################
 ;# Required Modules:                                                           #
@@ -41,232 +41,106 @@
 ;# Constants                                                                   #
 ;###############################################################################
 ;#Ports
-LED_PORT		EQU	PORTE
-LED_RED			EQU	PE1	
-LED_GREEN		EQU	PE0		
-LED_ALL			EQU	LED_GREEN|LED_RED
-LED_BUSY		EQU	LED_GREEN
-LED_ERR			EQU	LED_RED
+BACKLIGHT_PORT		EQU	PTT
+BACKLIGHT_DDR		EQU	DDRT
+BACKLIGHT_PIN		EQU	PT5
 
 ;#Timer channels
-LED_OC			EQU	6		;delay timer OC6
-
-;#Error status
-LED_STATE_DLYCNT    	EQU	$FFF8	 	;delay counter
-LED_STATE_ERR     	EQU	$0004	 	;untimed error
-LED_STATE_ERRBEEP	EQU	$0002	 	;error beep
-LED_STATE_COMERR	EQU	$0001	 	;comunication error
-
-;#Error signals
-LED_ERRBEEP_CNT		EQU	-190<<3		;error beep:    2 sec
-LED_COMERR_ON_CNT	EQU 	 -95<<3		;com error on:  0.5 sec
-LED_COMERR_OFF_CNT	EQU	-190<<3		;com error off: 1 sec
+BACKLIGHT_OC		EQU	5		;OC5
 	
 ;###############################################################################
 ;# Variables                                                                   #
 ;###############################################################################
-#ifdef LED_VARS_START_LIN
-			ORG 	LED_VARS_START, LED_VARS_START_LIN
+#ifdef BACKLIGHT_VARS_START_LIN
+			ORG 	BACKLIGHT_VARS_START, BACKLIGHT_VARS_START_LIN
 #else
-			ORG 	LED_VARS_START
-LED_VARS_START_LIN	EQU	@			
+			ORG 	BACKLIGHT_VARS_START
+BACKLIGHT_VARS_START_LIN	EQU	@			
 #endif	
-			ALIGN	1
-
-			;#Delay counter and flags
-LED_STATE		DS	2		;value of the SCIBD register *LED_BMUL
 	
-LED_VARS_END		EQU	*
-LED_VARS_END_LIN	EQU	@
+BACKLIGHT_VARS_END		EQU	*
+BACKLIGHT_VARS_END_LIN	EQU	@
 
 ;###############################################################################
 ;# Macros                                                                      #
 ;###############################################################################
 ;#Initialization
-#macro	LED_INIT, 0
-			;Initialize LEDs
-			;LED_OFF			;turn all LEDs off
+#macro	BACKLIGHT_INIT, 0
+			;Initialize GPIO
+			;BCLR	BACKLIGHT_PORT, #BACKLIGHT_PIN
+			;BSET	BACKLIGHT_DDR,  #BACKLIGHT_PIN
 			;Initialize timer
-			BSET	TIOS, #(1<<LED_OC)
-			;BCLR	TCTL2, #(3<<(2*(LED_OC-4)))
-			;BCLR	TIE, #(1<<LED_OC)
-			;Initialize variable
-			MOVW	#$0000,	LED_STATE
+			BSET	TIOS,  #(1<<BACKLIGHT_OC) 	;configure OC
+			BSET	TTOV,  #(1<<BACKLIGHT_OC) 	;toggle on overflow
+			;BCLR	TCTL1, #(1<<(2*(BACKLIGHT_OC-4)))
+			BSET	TCTL1, #(1<<(2*(BACKLIGHT_OC-4))+1)
+			;BCLR	TIE,   #(1<<BACKLIGHT_OC) 	;disable interrupts
+			;BSET	OCPD,  #(1<<BACKLIGHT_OC) 	;disable port output
 #emac
 	
-;# Busy Signal #################################################################
-;#Set busy signal
-; args:   none
+;#Set backlight brightness
+; args:   D: brightness
 ; result: none
-; SSTACK: none
+; SSTACK: 4 bytes
 ;         X, Y, and D are preserved 
-#macro	LED_BUSY_ON, 0
-			BCLR	LED_PORT, #LED_BUSY 	;turn LED on
-#emac
-	
-;#Clear busy signal
-; args:   none
-; result: none
-; SSTACK: none
-;         X, Y, and D are preserved 
-#macro	LED_BUSY_OFF, 0
-			BSET	LED_PORT, #LED_BUSY	;turn LED off
-#emac
-
-;# Error Signal ################################################################
-;#Set untimed error signal
-; args:   none
-; result: none
-; SSTACK: none
-;         X, Y, and D are preserved 
-#macro	LED_ERR_ON, 0
-			BSET	LED_STATE+1, #LED_STATE_ERR		;set signal status
-			BCLR	LED_PORT, #LED_ERR			;turn LED on
-			TIM_DIS	LED_OC 					;disable timer
-#emac
-	
-;#Clear untimed error signal
-; args:   1: macro to start atomic code
-;         2: macro to end atomic code
-; result: none
-; SSTACK: none
-;         X, Y, and A are preserved 
-#macro	LED_ERR_OFF, 2
-			\1		 				;start atomic code section
-			;Update status
-			LDAB	LED_STATE+1				;flags -> B
-			ANDB 	#(~(LED_STATE_ERR|LED_STATE_ERRBEEP))	;clear untimed error and beep request
-			;Check for communication error (flags in B)
-			BITB	#LED_STATE_COMERR			;check if untimed error or is active
-			BEQ	DONE					;untimed error is active
-			MOVB	#(LED_COMERR_OFF_CNT>>8), LED_STATE 	;set counter
-			ANDB	#(LED_STATE_DLYCNT&$FF)	
-			ORAB	#(LED_COMERR_OFF_CNT&LED_STATE_DLYCNT&$FF)
-			MOVW	TCNT, (TC0+(2*LED_OC)) 			;enable timer
-			TIM_EN	LED_OC	
-			;Turn off LED (flags in B)
-DONE			BSET	LED_PORT, #LED_ERR			;turn LED off
-			STAB	LED_STATE+1				;set state
-			\2	 					;end atomic code section
-#emac
-
-;#Error beep replacement
-; args:   1: macro to start atomic code
-;         2: macro to end atomic code
-; result: none
-; SSTACK: none
-;         X and Y are preserved 
-#macro	LED_ERRBEEP, 2
-			\1		 				;start atomic code section
-			;Check for untimed error nested beep 
-			LDAB	LED_STATE+1				;flags -> B
-			BITB	#(LED_STATE_ERR|LED_STATE_ERRBEEP)	;check if untimed error or is active
-			BNE	DONE					;untimed error is active
-			;Start error beep (flags in B) 
-			LDAA	#(LED_ERRBEEP_CNT>>8) 			;set counter
-			ANDB	#(LED_STATE_DLYCNT&$FF)	
-			ORAB	#(LED_ERRBEEP_CNT&LED_STATE_DLYCNT&$FF)
-			MOVW	TCNT, (TC0+(2*LED_OC)) 			;enable timer
-			TIM_EN	LED_OC	
-			BCLR	LED_PORT, #LED_ERR			;turn LED on
-			STD	LED_STATE				;set state
-DONE			\2	 					;end atomic code section
-#emac
-	
-;#Set comunication error signal
-; args:   1: macro to start atomic code
-;         2: macro to end atomic code
-; result: none
-; SSTACK: none
-;         X and Y are preserved 
-#macro	LED_COMERR_ON, 2
-			\1		 				;start atomic code section
-			;Check for running error signals 
-			LDAB	LED_STATE+1				;flags -> B
-			BITB	#(LED_STATE_ERR|LED_STATE_ERRBEEP|LED_STATE_COMERR);check if untimed error or is active
-			BNE	DONE					;untimed error is active
-			;Start error beep (flags in B)
-			LDAA	#(LED_COMERR_ON_CNT>>8) 		;set counter
-			ANDB	#(LED_STATE_DLYCNT&$FF)	
-			ORAB	#(LED_COMERR_ON_CNT&LED_STATE_DLYCNT&$FF)
-			MOVW	TCNT, (TC0+(2*LED_OC)) 			;enable timer
-			TIM_EN	LED_OC	
-			BCLR	LED_PORT, #LED_ERR			;turn LED on
-			STD	LED_STATE				;set state
-DONE			\2	 					;end atomic code section
-#emac
-	
-;#Clear comunication error signal
-; args:   none
-; result: none
-; SSTACK: none
-;         X, Y, and D are preserved 
-#macro	LED_COMERR_OFF, 0
-			BCLR	LED_STATE, #LED_STATE_COMERR		;set signal status
-#emac
-
-;#Empty macro
-; args:   none
-; result: none
-; SSTACK: none
-;         X, Y, and D are preserved 
-#macro	LED_NOP, 0
+#macro	BACKLIGHT_SET, 0
+			SSTACK_JOBSR	BACKLIGHT_SET, 4
 #emac
 	
 ;###############################################################################
 ;# Code                                                                        #
 ;###############################################################################
-#ifdef LED_CODE_START_LIN
-			ORG 	LED_CODE_START, LED_CODE_START_LIN
+#ifdef BACKLIGHT_CODE_START_LIN
+			ORG 	BACKLIGHT_CODE_START, BACKLIGHT_CODE_START_LIN
 #else
-			ORG 	LED_CODE_START
-LED_CODE_START_LIN	EQU	@			
+			ORG 	BACKLIGHT_CODE_START
+BACKLIGHT_CODE_START_LIN	EQU	@			
 #endif	
 
-;#Timer triggered ISR
-LED_ISR			EQU	*
-			;Clear interrupt flag
-			TIM_CLRIF	LED_OC	
-			;Increment counter
-			LDD	LED_STATE 				;increment delay counter
-			ADDD	#$0008
-			BCC	LED_ISR_4 				;no overflow
-			;End of delay (state in D)
-			ANDB	#~LED_STATE_ERRBEEP 			;clear error beep flag
-			BITB	#LED_STATE_COMERR 			;check if com error is active
-			BEQ	LED_ISR_2 				;com error is inactive 
-			;Check LED status (state in D)
-			ANDB	#(LED_STATE_DLYCNT&$FF)			;clear delay count
-			BRCLR	LED_PORT, #LED_ERR, LED_ISR_1		;LED is on
-			;Turn LED on (state in D)
-			LDAA	#(LED_COMERR_ON_CNT>>8) 		;set counter
-			ORAB	#(LED_COMERR_ON_CNT&LED_STATE_DLYCNT&$FF)
-			BCLR	LED_PORT, #LED_ERR			;turn LED on
-			JOB	LED_ISR_4
-			;Turn LED off (state in D)
-LED_ISR_1		LDAA	#(LED_COMERR_OFF_CNT>>8) 		;set counter
-			ORAB	#(LED_COMERR_OFF_CNT&LED_STATE_DLYCNT&$FF)
-			JOB	LED_ISR_3
-			;Turn off error LED 
-LED_ISR_2		TIM_DIS	LED_OC 					;disable timer	
-LED_ISR_3		BSET	LED_PORT, #LED_ERR			;turn LED off
-LED_ISR_4		STD	LED_STATE				;update state
-			;Done
-			ISTACK_RTI
-	
-LED_CODE_END		EQU	*	
-LED_CODE_END_LIN	EQU	@	
+;#Set backlight brightness
+; args:   D: brightness
+; result: none
+; SSTACK: 2 bytes
+;         X, Y, and D are preserved 
+BACKLIGHT_SET			EQU	*
+				;Check for zero (brightness in D) 
+				TBEQ	D, BACKLIGHT_SET_2 	;turn backlight off
+				IBEQ	D, BACKLIGHT_SET_5	;turn on full brightness
+				;Enable PWM (brightness-1 in D) 
+				SUBD	#1 			;restore brightness value
+				STD	(TC0+(2*BACKLIGHT_OC))	;set brightness
+				BCLR	OCPD, #(1<<BACKLIGHT_OC);enable port output
+				MOVB	#(TEN|TSFRZ), TSCR1	;enable timer
+				;Done (brightness in D) 
+BACKLIGHT_SET_1			SSTACK_PREPULL	2
+				RTS
+				;Turn backlight off
+BACKLIGHT_SET_2			BCLR	BACKLIGHT_PORT, #BACKLIGHT_PIN
+BACKLIGHT_SET_3			BSET	OCPD,  #(1<<BACKLIGHT_OC) 	;disable port output
+				TST	TIE
+				BNE	BACKLIGHT_SET_1
+				BRSET	OCPD, #$FF, BACKLIGHT_SET_4
+				JOB	BACKLIGHT_SET_1
+BACKLIGHT_SET_4			CLR	TSCR1 			;disable timer
+				JOB	BACKLIGHT_SET_1
+				;Turn on full brightness
+BACKLIGHT_SET_5			SUBD	#1 			;restore brightness value
+				BSET	BACKLIGHT_PORT, #BACKLIGHT_PIN
+				JOB	BACKLIGHT_SET_3
+
+BACKLIGHT_CODE_END		EQU	*	
+BACKLIGHT_CODE_END_LIN	EQU	@	
 
 ;###############################################################################
 ;# Tables                                                                      #
 ;###############################################################################
-#ifdef LED_TABS_START_LIN
-			ORG 	LED_TABS_START, LED_TABS_START_LIN
+#ifdef BACKLIGHT_TABS_START_LIN
+			ORG 	BACKLIGHT_TABS_START, BACKLIGHT_TABS_START_LIN
 #else
-			ORG 	LED_TABS_START
-LED_TABS_START_LIN	EQU	@			
+			ORG 	BACKLIGHT_TABS_START
+BACKLIGHT_TABS_START_LIN	EQU	@			
 #endif	
 
-LED_TABS_END		EQU	*	
-LED_TABS_END_LIN	EQU	@	
+BACKLIGHT_TABS_END		EQU	*	
+BACKLIGHT_TABS_END_LIN	EQU	@	
 #endif	
